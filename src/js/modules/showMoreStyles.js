f7bead534151195277fe9255da798654d715ce97
Denis <example@gmail.com>
2023-01-03 23:33:12 +0300

    show more cards

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,18 +1,40 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles),
-          btn = document.querySelector(trigger);
+import { getResource } from "../services/requests";
 
-    cards.forEach(card => {
-        card.classList.add('animated', 'FadeInUp');
+const showMoreStyles = (trigger, wrapper) => {
+    const btn = document.querySelector(trigger);
+
+    function errorMessage() {
+        let statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        statusMessage.textContent = 'Что-то пошло не так...';
+        document.querySelector(wrapper).appendChild(statusMessage);
+    }
+    
+    btn.addEventListener('click', function() {
+        getResource('assets/db.json')
+            .then(res => createCards(res.styles))
+            .catch(() => errorMessage());
+
+        this.remove();
     });
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    function createCards(response) {
+        response.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'FadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+            card.innerHTML = `
+                <div class=styles-block>
+                    <img src=${src} alt="style">
+                    <h4>${title}</h4>
+                    <a href=${link}>Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);           
         });
-        btn.remove();
-    });
+    }
 };
 
 export default showMoreStyles;
